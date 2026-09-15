bd80e652a6c2ac38c0a0755548fa04accfab7997
CCN-Support <ccnsupport@g.ucla.edu>
2026-09-15 13:52:31 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,14 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 
+# -- Python for Custom CSS file
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
 
